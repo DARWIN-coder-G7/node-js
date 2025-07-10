25a467956b8767dbb8fbf0273cc2477dc5b56f92
Darwin <sakthiveldarwin@gmail.com>
2025-07-10 14:40:17 +0530

    new dependency

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 console.log("testing");
 console.log("testing is nodemon working");
+
+const {format}= require('date-fns');
+const {v4: uuid}= require('uuid');
+
+console.log(format(new Date(),'ddMMyyyy\tHH:mm:ss'));
+console.log(uuid());
