7ec930f1bd9b51a43c678dae57e1775987dc0240
Darwin <sakthiveldarwin@gmail.com>
2025-07-14 11:08:37 +0530

    started server logics

--- server.js
+++ server.js
@@ -1,35 +1,14 @@
-// import { readFile } from 'node:fs';
+const http=require('http');
 
-const fs = require('fs').promises;
-const path = require('path');
+const PORT = 3000;
 
-const fileOps = async () => {
-  try {
-    const data = await fs.readFile(path.join(__dirname, 'files', 'start.txt'), 'utf8');
-    console.log(data);
-    await fs.writeFile(path.join(__dirname, 'files', 'first.txt'),
-      'Hi This is the First text File created by Node Js',
-      console.log('Write Completed'));
+const server = http.createServer((req,res)=>{
+  res.write("HI HEllo FRom Node JS")
+  res.end()
 
-    await fs.appendFile(path.join(__dirname, 'files', 'first.txt'), '\n\n Thank YOU',
-      console.log('Append Completed'));
-
-    fs.rename(path.join(__dirname, 'files', 'first.txt'), path.join(__dirname, 'files', 'testingRename.txt'),
-      console.log('Rename Completed'));
-
-      fs.unlink(path.join(__dirname, 'files', 'start.txt'));
-
-  } catch (err) {
-    console.error(err);
-  }
-}
-
-fileOps();
-
-// readFile is an Asynchronous function
-
-//Exit on UnCaught Errors
-process.on('uncaughtException', err => {
-  console.error(`There was an UnHandled Exception Occured : ${err}`)
-  process.exit(1)
 });
+
+server.listen(PORT,(err)=>{
+  if(err) console,log(err)
+    else console.log(`Server is Running SuccessFully on Port ${PORT}`)
+  })
